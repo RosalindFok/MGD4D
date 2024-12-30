5964082b71c5e098c72571dffc61d8c4117f8573
Rosalind Fok <huoyufu19@mails.ucas.ac.cn>
2024-12-30 12:03:54 +0800

    2datasets

--- README.md
+++ README.md
@@ -1,23 +1,21 @@
 # MGD4MD
 Multi-modal Graph Diffusion for identifying neuroimaging biomarkers of mental disorder
 
-## Windows11上配准
+## 1. Preprocess
+Platform: Windows11, Python 3.11.4
 ``` shell
+# dependencies
+pip3 install numpy
 pip install antspyx
 pip install nibabel
 pip install pandas
 pip install tqdm
 pip install matplotlib
-```
+pip install nilearn
 
-通用预处理步骤
-1. 去除头骨
-2. 时间层校正(Slice-Timing Correction):旨在解决MRI扫描中由于切片采集顺序不同而导致的时间差问题
-3. 配准与归一化(Registration and Normalization):将每个被试的大脑图像都映射到同一个标准空间
-4. 对齐与运动校正(Alignment and Motion Correction):纠正被试在扫描过程中可能产生的头部运动
-5. 平滑(Smoothing):对图像进行平均处理来减少噪音，提升信号的质量
-6. 掩膜(or 掩码)与缩放(Masking and Scaling):用于生成头部掩膜，并对数据进行缩放处理，以确保数据的一致性
-7. 检查预处理结果:
+# script
+python fMRI2Graph.py
+```
 
 
 ## Prepare the Enviorment:
@@ -33,8 +31,8 @@ chmod 777 *.sh
 sbatch --gpus=num_gpus -p gpu run.sh # submit the job
 parajobs # check id of the job
 scancel job_id # cancel the job via its id
-```
 
 pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121 
 pip install nibabel -i https://pypi.tuna.tsinghua.edu.cn/simple/ 
 
+```
--- fMRI2Graph.py
+++ fMRI2Graph.py
@@ -1,6 +1,7 @@
 import ants
 import json
 import time
+import shutil
 import numpy as np
 import pandas as pd 
 import nibabel as nib 
@@ -10,6 +11,12 @@ from nilearn import maskers, connectome
 
 from path import Path, Paths
 
+def clear_temporary_files_of_ants(temp_dir_path : Path = Path("C:") / "Users" / "26036" / "AppData"/ "Local" / "Temp") -> None:
+    # TODO: set your own temp_dir_path.
+    for suffix in ["*.nii.gz", "*.mat"]:
+        for file_path in temp_dir_path.glob(suffix):
+            file_path.unlink(missing_ok=True)
+       
 def register(fixed_path : str, moving_path : str, do_resample : bool = True) -> ants.ANTsImage:
     fixed_image = ants.image_read(fixed_path)
     moving_image = ants.image_read(moving_path)
@@ -41,128 +48,192 @@ def adjust_dim_of_antsImage(image_5d_path : Path) -> None:
         end_time = time.time()
         print(f"It took {end_time - start_time:.2f} seconds to adjust dim of {image_5d_path.absolute()}")
 
-def process_ds002748() -> None:
+def preprocess_anat3d_and_func4d_with_atlas(saved_dir_path : Path, 
+                                            participants_info : pd.DataFrame,
+                                            anat3d_path : Path, func4d_path : Path, 
+                                            atlas_path : Path = Paths.BN_Atlas_246_1mm_nii_path
+                                        ) -> None:
+    # Save participant's information
+    info_json_path = saved_dir_path / "info.json"
+    if not info_json_path.exists():
+        subject_info = participants_info[participants_info['participant_id'] == saved_dir_path.name]  
+        subject_info = subject_info.iloc[0].to_dict()
+        with open(info_json_path, 'w') as f:
+            json.dump(subject_info, f, indent=4)
+
+    # Step 1: Register
+    # anat: anat -> atlas 
+    aligned_anat_path = saved_dir_path / "aligned_anat.nii.gz"
+    if not aligned_anat_path.exists():
+        print(f"Registering {saved_dir_path.name}: {anat3d_path.parent.name}")
+        result = register(fixed_path=str(atlas_path), moving_path=str(anat3d_path))
+        ants.image_write(result, str(aligned_anat_path))
+        del result
+    # func: func -> anat
+    aligned_func_path = saved_dir_path / "aligned_func.nii.gz"
+    if not aligned_func_path.exists():
+        moving_image = ants.image_read(str(func4d_path))
+        results_list = [] 
+        for t in tqdm(range(moving_image.shape[-1]), desc=f"Registering {saved_dir_path.name}: {func4d_path.parent.name}", leave=True):
+            # delete the first 5 frames
+            if t < 5:
+                continue
+            # t:t+1 cannot be t, and ":,:,:," cannot be "...,", otherwise the orientation will be wrong
+            moving_image_t = moving_image[:, :, :, t:t+1]
+            temp_path = split_4dantsImageFrame_into_3d(image_4d=moving_image_t)
+            # register
+            result = register(fixed_path=str(aligned_anat_path), moving_path=temp_path)
+            results_list.append(result)
+            # delete temporary file and variables
+            Path(temp_path).unlink()
+            del result, moving_image_t, temp_path
+        ants.image_write(ants.merge_channels(results_list), str(aligned_func_path))
+        del moving_image, results_list
+    # change the dim[0] from 5 to 4
+    adjust_dim_of_antsImage(aligned_func_path)
+    # Clear the temporary files of ants
+    clear_temporary_files_of_ants()
+
+    # Step 2: Denoise
+    # anat 
+    denoised_anat_path = saved_dir_path / "denoised_anat.nii.gz"
+    if not denoised_anat_path.exists():
+        print(f"Denoising {denoised_anat_path.parent.name}: anat")
+        result = ants.denoise_image(image=ants.image_read(str(aligned_anat_path)))
+        ants.image_write(result, str(denoised_anat_path))
+        del result
+    # func
+    denoised_func_path = saved_dir_path / "denoised_func.nii.gz"
+    if not denoised_func_path.exists():
+        aligned_func = ants.image_read(str(aligned_func_path))
+        results_list = [] 
+        for t in tqdm(range(aligned_func.shape[-1]), desc=f"Denoising {denoised_func_path.parent.name}: func", leave=True):
+            aligned_func_t = aligned_func[:, :, :, t:t+1]
+            temp_path = split_4dantsImageFrame_into_3d(image_4d=aligned_func_t)
+            result = ants.denoise_image(ants.image_read(str(temp_path)))
+            results_list.append(result)
+            # delete temporary file and variables
+            Path(temp_path).unlink()
+            del result, aligned_func_t, temp_path
+        ants.image_write(ants.merge_channels(results_list, channels_first=True), str(denoised_func_path))
+        del aligned_func, results_list
+    # change the dim[0] from 5 to 4
+    adjust_dim_of_antsImage(denoised_func_path)
+
+    # Step 3: Functional connectivity
+    fc_matrix_path = saved_dir_path / "fc_matrix.npy"
+    if not fc_matrix_path.exists():
+        start_time = time.time()
+        atlas = nib.load(Paths.BN_Atlas_246_1mm_nii_path)
+        denoised_func = nib.load(denoised_func_path)
+        masker = maskers.NiftiLabelsMasker(labels_img=atlas, standardize="zscore_sample")
+        time_series = masker.fit_transform(denoised_func)
+        matrix = connectome.ConnectivityMeasure(kind="correlation", standardize="zscore_sample").fit_transform([time_series])[0]
+        np.save(fc_matrix_path, matrix)
+        # plot heatmap
+        plt.figure(figsize=(10, 10))
+        plt.imshow(matrix, cmap="RdBu_r", interpolation="nearest")  
+        plt.colorbar()  
+        plt.title("Heatmap")  
+        plt.xlabel("X-axis")  
+        plt.ylabel("Y-axis")  
+        plt.savefig(saved_dir_path / "correlation_matrix.svg", format="svg")  
+        plt.close()  
+        end_time = time.time()
+        print(f"It took {end_time - start_time:.2f} seconds to calculate the functional connectivity matrix.")
+
+
+def process_ds002748(dir_path : Path = Paths.ds002748_dir_path) -> None:
     """
+    fMRI
     72 subjects = 51 subjects with mild depression + 21 healthy controls
     A session consists of 100 dynamic scans with TR = 2.5 s and 25 slices. 
     anat: shape=[288,288,181], voxel size=1mm * 1mm * 1mm
     func: shape=[112,112,25,100], voxel size=2mm * 2mm * 5mm
     """
-    assert Paths.ds003007_dir_path.exists(), f"{Paths.ds003007_dir_path.absolute()} does not exist!"
-    participants_tsv_path = Paths.ds002748_dir_path / "participants.tsv"
+    assert dir_path.exists(), f"{dir_path.absolute()} does not exist!"
+    participants_tsv_path = dir_path / "participants.tsv"
     assert participants_tsv_path.exists(), f"{participants_tsv_path.absolute()} does not exist!"
     participants_info = pd.read_csv(participants_tsv_path, sep='\t')
-    sub_dir_path_list = [d for d in Paths.ds002748_dir_path.iterdir() if d.is_dir()]
+    sub_dir_path_list = [d for d in dir_path.iterdir() if d.is_dir()]
     assert len(sub_dir_path_list) == len(participants_info), f"len(sub_dir_path_list)={len(sub_dir_path_list)} != len(participants_info)={len(participants_info)}"
     sub_anat_nii_path_dict = {d.name : p for d in sub_dir_path_list for p in (d / "anat").iterdir()}
     sub_func_nii_path_dict = {d.name : p for d in sub_dir_path_list for p in (d / "func").iterdir()}
-
 
     for sub_name in sub_anat_nii_path_dict.keys():
         # path of saved dir, each subject has its own dir
         saved_dir_path = Paths.run_files_ds002748_dir_path / sub_name
         saved_dir_path.mkdir(parents=True, exist_ok=True)
-       
-        # Save participant's information
-        info_json_path = saved_dir_path / "info.json"
-        if not info_json_path.exists():
-            subject_info = participants_info[participants_info['participant_id'] == sub_name]  
-            subject_info = subject_info.iloc[0].to_dict()
-            with open(info_json_path, 'w') as f:
-                json.dump(subject_info, f, indent=4)
 
-        # Step 1: Register
-        # anat: anat -> atlas 
-        aligned_anat_path = saved_dir_path / "aligned_anat.nii.gz"
-        if not aligned_anat_path.exists():
-            print(f"Registering {sub_name}: anat")
-            result = register(fixed_path=str(Paths.BN_Atlas_246_1mm_nii_path), moving_path=str(sub_anat_nii_path_dict[sub_name]))
-            ants.image_write(result, str(aligned_anat_path))
-            del result
-        # func: func -> anat
-        aligned_func_path = saved_dir_path / "aligned_func.nii.gz"
-        if not aligned_func_path.exists():
-            moving_image = ants.image_read(str(sub_func_nii_path_dict[sub_name]))
-            results_list = [] 
-            for t in tqdm(range(moving_image.shape[-1]), desc=f"Registering {sub_name}: func", leave=True):
-                # delete the first 5 frames
-                if t < 5:
-                    continue
-                # t:t+1 cannot be t, and ":,:,:," cannot be "...,", otherwise the orientation will be wrong
-                moving_image_t = moving_image[:, :, :, t:t+1]
-                temp_path = split_4dantsImageFrame_into_3d(image_4d=moving_image_t)
-                # register
-                result = register(fixed_path=str(aligned_anat_path), moving_path=temp_path)
-                results_list.append(result)
-                # delete temporary file and variables
-                Path(temp_path).unlink()
-                del result, moving_image_t, temp_path
-            ants.image_write(ants.merge_channels(results_list), str(aligned_func_path))
-            del moving_image, results_list
-        # target dim=[4 182 218 182  95   1   1   1]
-        adjust_dim_of_antsImage(aligned_func_path)
-
-        # Clear the temporary files of ants
-        temp_dir_path = Path("C:") / "Users" / "26036" / "AppData"/ "Local" / "Temp"
-        assert temp_dir_path.exists(), f"{temp_dir_path.absolute()} does not exist!"
-        [file_path.unlink(missing_ok=True) for suffix in ["*.nii.gz", "*.mat"] for file_path in temp_dir_path.glob(suffix)]
+        preprocess_anat3d_and_func4d_with_atlas(saved_dir_path=saved_dir_path, 
+                                                participants_info=participants_info,
+                                                anat3d_path=sub_anat_nii_path_dict[sub_name], 
+                                                func4d_path=sub_func_nii_path_dict[sub_name],
+                                                atlas_path=Paths.BN_Atlas_246_1mm_nii_path)        
         
-        # Step 2: Denoise
-        # anat 
-        denoised_anat_path = saved_dir_path / "denoised_anat.nii.gz"
-        if not denoised_anat_path.exists():
-            print(f"Denoising {sub_name}: anat")
-            result = ants.denoise_image(image=ants.image_read(str(aligned_anat_path)))
-            ants.image_write(result, str(denoised_anat_path))
-            del result
-        # func
-        denoised_func_path = saved_dir_path / "denoised_func.nii.gz"
-        if not denoised_func_path.exists():
-            aligned_func = ants.image_read(str(aligned_func_path))
-            results_list = [] 
-            for t in tqdm(range(aligned_func.shape[-1]), desc=f"Denoising {sub_name}: func", leave=True):
-                aligned_func_t = aligned_func[:, :, :, t:t+1]
-                temp_path = split_4dantsImageFrame_into_3d(image_4d=aligned_func_t)
-                result = ants.denoise_image(ants.image_read(str(temp_path)))
-                results_list.append(result)
-                # delete temporary file and variables
-                Path(temp_path).unlink()
-                del result, aligned_func_t, temp_path
-            ants.image_write(ants.merge_channels(results_list, channels_first=True), str(denoised_func_path))
-            del aligned_func, results_list
-        # target dim=[4 182 218 182  95   1   1   1]
-        adjust_dim_of_antsImage(denoised_func_path)
 
-        # Step 3: Functional connectivity
-        fc_matrix_path = saved_dir_path / "fc_matrix.npy"
-        if not fc_matrix_path.exists():
-            start_time = time.time()
-            atlas = nib.load(Paths.BN_Atlas_246_1mm_nii_path)
-            denoised_func = nib.load(denoised_func_path)
-            masker = maskers.NiftiLabelsMasker(labels_img=atlas, standardize="zscore_sample")
-            time_series = masker.fit_transform(denoised_func)
-            matrix = connectome.ConnectivityMeasure(kind="correlation", standardize="zscore_sample").fit_transform([time_series])[0]
-            np.save(fc_matrix_path, matrix)
-            # plot heatmap
-            plt.imshow(matrix, cmap="RdBu_r", interpolation="nearest")  
-            plt.colorbar()  
-            plt.title("Heatmap")  
-            plt.xlabel("X-axis")  
-            plt.ylabel("Y-axis")  
-            plt.savefig(saved_dir_path / "correlation_matrix.svg", format="svg")   
-            end_time = time.time()
-            print(f"It took {end_time - start_time:.2f} seconds to calculate the functional connectivity matrix.")
+def process_ds003007(dir_path : Path = Paths.ds003007_dir_path) -> None:
+    """
+    fMRI
+    29 subjects = 15 depr_no_treatment + 8 depr_cbt + 6 depr_nfb
+    depr_no_treatment: untreated patients with depression
+    depr_cbt: patients who undergo cognitive behavioral therapy
+    depr_nfb: patients who undergo rt-fmri neurofeedback treatment course
+    There are two session of RS with 2-month interval, 3 groups of patients: no treatment, CBT, or fmri-NFB treatment. 
+    A session consists of 100 dynamic scans with TR = 2.5 s and 25 slices.
+    ses-pre: anat and func
+    ses-post: depr_no_treatment only have func, depr_cbt and depr_nfb have both anat and func
+    """
+    assert dir_path.exists(), f"{dir_path.absolute()} does not exist!"
+    participants_tsv_path = dir_path / "participants.tsv"
+    assert participants_tsv_path.exists(), f"{participants_tsv_path.absolute()} does not exist!"
+    participants_info = pd.read_csv(participants_tsv_path, sep='\t')
+    sub_dir_path_list = [d for d in dir_path.iterdir() if d.is_dir()]
+    assert len(sub_dir_path_list) == len(participants_info), f"len(sub_dir_path_list)={len(sub_dir_path_list)} != len(participants_info)={len(participants_info)}"
+    
+    for d in sub_dir_path_list:
+        # pre
+        saved_pre_dir_path = Paths.run_files_ds003007_dir_path / "pre" / d.name
+        saved_pre_dir_path.mkdir(parents=True, exist_ok=True)
+        pre_anat_file_path = [x for x in (d / "ses-pre" / "anat").glob("*")]
+        assert len(pre_anat_file_path) == 1, f"len(pre_anat_file_path)={len(pre_anat_file_path)} != 1"
+        pre_anat_file_path = pre_anat_file_path[0]
+        pre_func_file_path = [x for x in (d / "ses-pre" / "func").glob("*")]
+        assert len(pre_func_file_path) == 1, f"len(pre_func_file_path)={len(pre_func_file_path)} != 1"
+        pre_func_file_path = pre_func_file_path[0]
+        preprocess_anat3d_and_func4d_with_atlas(saved_dir_path=saved_pre_dir_path, 
+                                                participants_info=participants_info,
+                                                anat3d_path=pre_anat_file_path, 
+                                                func4d_path=pre_func_file_path,
+                                                atlas_path=Paths.BN_Atlas_246_1mm_nii_path) 
 
+        # post
+        saved_post_dir_path = Paths.run_files_ds003007_dir_path / "post" / d.name
+        saved_post_dir_path.mkdir(parents=True, exist_ok=True)
+        post_func_file_path = [x for x in (d / "ses-post" / "func").glob("*")]
+        assert len(post_func_file_path) == 1, f"len(post_func_file_path)={len(post_func_file_path)} != 1"
+        post_func_file_path = post_func_file_path[0]
+        if not (d / "ses-post" / "anat").exists():
+            for src in saved_pre_dir_path.glob("*anat*"):
+                shutil.copy(src=src, dst=saved_post_dir_path / src.name)
+            preprocess_anat3d_and_func4d_with_atlas(saved_dir_path=saved_post_dir_path, 
+                                                    participants_info=participants_info,
+                                                    anat3d_path=None, 
+                                                    func4d_path=post_func_file_path,
+                                                    atlas_path=Paths.BN_Atlas_246_1mm_nii_path)
+        else:
+            post_anat_file_path = [x for x in (d / "ses-post" / "anat").glob("*")]
+            assert len(post_anat_file_path) == 1, f"len(post_anat_file_path)={len(post_anat_file_path)} != 1"
+            post_anat_file_path = post_anat_file_path[0]
+            preprocess_anat3d_and_func4d_with_atlas(saved_dir_path=saved_post_dir_path,
+                                                    participants_info=participants_info,
+                                                    anat3d_path=post_anat_file_path,
+                                                    func4d_path=post_func_file_path,
+                                                    atlas_path=Paths.BN_Atlas_246_1mm_nii_path)
 
-# def process_ds003007() -> None:
-#     """
+def main() -> None:
+    process_ds002748()
+    process_ds003007()
 
-#     """
-#     assert Paths.ds003007_dir_path.exists(), f"{Paths.ds003007_dir_path.absolute()} does not exist!"
-#     participants_tsv_path = Paths.ds003007_dir_path / "participants.tsv"
-#     assert participants_tsv_path.exists(), f"{participants_tsv_path.absolute()} does not exist!"
-#     participants_info = pd.read_csv(participants_tsv_path, sep='\t')
-#     sub_dir_path_list = [d for d in Paths.ds003007_dir_path.iterdir() if d.is_dir()]
-#     assert len(sub_dir_path_list) == len(participants_info), f"len(sub_dir_path_list)={len(sub_dir_path_list)} != len(participants_info)={len(participants_info)}"
-
-process_ds002748()
+if __name__ == "__main__":
+    main()
